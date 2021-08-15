7c78eddfdb7abf01a7ea887a295eb27d4700a5b3
pkinsella <patrick.kinsella@recordedfuture.com>
2021-08-15 14:24:06 -0600

    create layout and homepage templates

--- blog.py
+++ blog.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<p>Hello, World!</p>'
+@app.route('/blog')
+def blog():
+    return render_template('homepage.html')
